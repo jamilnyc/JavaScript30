6e843578c612be83919429b2983aeb8e624080d1
Murshed J. Ahmed <mahmed@marvel.com>
2022-02-06 18:25:40 -0500

    Exercise 02 | linted script.js

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -5,48 +5,47 @@ const hourHand = document.querySelector('.hour-hand');
 
 /**
  * Return the number of degrees to rotate the clock hand proportional to the current value.
- * 
- * @param {number} currentValue 
- * @param {number} maxValue 
- * @param {number} offset 
+ *
+ * @param {number} currentValue
+ * @param {number} maxValue
+ * @param {number} offset
  * @returns {number}
  */
 function getDegreesForHand(currentValue, maxValue, offset = 90) {
-    return (currentValue / maxValue) * 360 + offset;
+  return (currentValue / maxValue) * 360 + offset;
 }
 
 /**
  * Rotate the given hand to the given degrees, by updating the style.
- * 
- * @param {Element} hand 
- * @param {number} degrees 
+ *
+ * @param {Element} hand
+ * @param {number} degrees
  * @param {number} offset
  */
 function transformToDegrees(hand, degrees, offset = 90) {
-    if (degrees === offset) {
-        hand.classList.add('no-transition');
-        hand.offsetHeight; // trigger reflow
-    } else {
-        hand.classList.remove('no-transition');
-    }
-    hand.style.transform = `rotate(${degrees}deg)`;
-    
+  if (degrees === offset) {
+    hand.classList.add('no-transition');
+    // eslint-disable-next-line no-unused-expressions
+    hand.offsetHeight; // trigger reflow
+  } else {
+    hand.classList.remove('no-transition');
+  }
+  hand.style.transform = `rotate(${degrees}deg)`;
 }
 
 function setDate() {
-    const now = new Date();
-    const secondsDegrees = getDegreesForHand(now.getSeconds(), 60);
-    console.log(`Seconds Degrees: ${secondsDegrees}`);
-    transformToDegrees(secondHand, secondsDegrees);
+  const now = new Date();
+  const secondsDegrees = getDegreesForHand(now.getSeconds(), 60);
+  console.log(`Seconds Degrees: ${secondsDegrees}`);
+  transformToDegrees(secondHand, secondsDegrees);
 
-    const minutesDegrees = getDegreesForHand(now.getMinutes(), 60);
-    transformToDegrees(minuteHand, minutesDegrees);
+  const minutesDegrees = getDegreesForHand(now.getMinutes(), 60);
+  transformToDegrees(minuteHand, minutesDegrees);
 
-    const hoursDegrees = getDegreesForHand(now.getHours(), 12);
-    transformToDegrees(hourHand, hoursDegrees);
+  const hoursDegrees = getDegreesForHand(now.getHours(), 12);
+  transformToDegrees(hourHand, hoursDegrees);
 
-    
-    // console.log(`Hours: ${now.getHours()}, Degrees: ${hoursDegrees}`);
+  // console.log(`Hours: ${now.getHours()}, Degrees: ${hoursDegrees}`);
 }
 
 setDate();

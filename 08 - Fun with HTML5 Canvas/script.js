e67750ff2e17fb7dc94ca17e44d8548713aed9aa
Murshed J. Ahmed <mahmed@marvel.com>
2022-02-09 19:50:13 -0500

    Exercise 08 | attempt at making a speed-based width

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -16,7 +16,49 @@ let lastX = 0;
 let lastY = 0;
 
 let hue = 0;
-let direction = true;
+
+// Used for calculating mouse speed
+/** @type MouseEvent */
+let currentEvent;
+/** @type MouseEvent */
+let previousEvent;
+let currentMouseSpeed; // pixels per second
+let previousMouseSpeed;
+let currentAcceleration;
+
+const POLLING_INTERVAL_MS = 20;
+const MAX_LINE_WIDTH = 50;
+const MIN_LINE_WIDTH = 2;
+const MAX_SPEED = 12000;
+const MAX_WIDTH_DIFF = 10;
+
+const currentLineWidth = MIN_LINE_WIDTH;
+
+/**
+ * Return the width of the line, based on the current speed of the mouse.
+ *
+ * @returns {Number}
+ */
+function getLineWidth(previousLineWidth) {
+  if (!currentMouseSpeed) {
+    console.warn('Mouse Speed Unknown');
+    return MIN_LINE_WIDTH;
+  }
+  const adjustedSpeed = Math.min(currentMouseSpeed, MAX_SPEED);
+  let width = Math.floor(
+    MAX_LINE_WIDTH - (MAX_LINE_WIDTH * adjustedSpeed) / MAX_SPEED
+  );
+  width = Math.max(MIN_LINE_WIDTH, width);
+  return width;
+
+  //   if (currentAcceleration > 0) {
+  //     return Math.min(MAX_LINE_WIDTH, previousLineWidth + 5);
+  //   }
+  //   if (currentAcceleration < 0) {
+  //     return Math.max(MIN_LINE_WIDTH, previousLineWidth - 5);
+  //   }
+  //   return previousLineWidth;
+}
 
 /**
  * @param {MouseEvent} e
@@ -25,7 +67,6 @@ function draw(e) {
   if (!isDrawing) {
     return;
   }
-  console.log(e);
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
@@ -39,15 +80,7 @@ function draw(e) {
     hue = 0;
   }
 
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-    direction = !direction;
-  }
-
-  if (direction) {
-    ctx.lineWidth += 1;
-  } else {
-    ctx.lineWidth -= 1;
-  }
+  ctx.lineWidth = getLineWidth(currentLineWidth);
 }
 
 /**
@@ -60,7 +93,32 @@ function handleMouseDown(e) {
   [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
-canvas.addEventListener('mousemove', draw);
+/**
+ * @param {MouseEvent} e
+ */
+function handleMouseMove(e) {
+  draw(e);
+  currentEvent = e;
+}
+
+setInterval(() => {
+  // Need two data points to estimate speed
+  if (previousEvent && currentEvent) {
+    const deltaX = currentEvent.screenX - previousEvent.screenX;
+    const deltaY = currentEvent.screenY - previousEvent.screenY;
+    const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
+
+    previousMouseSpeed = currentMouseSpeed;
+    currentMouseSpeed = distance / (POLLING_INTERVAL_MS / 1000);
+
+    currentAcceleration =
+      (currentMouseSpeed - previousMouseSpeed) / (POLLING_INTERVAL_MS / 1000);
+    console.log(`Mouse Speed: ${currentMouseSpeed} px/sec`);
+  }
+  previousEvent = currentEvent;
+}, POLLING_INTERVAL_MS);
+
+canvas.addEventListener('mousemove', handleMouseMove);
 canvas.addEventListener('mousedown', handleMouseDown);
 
 canvas.addEventListener('mouseup', () => (isDrawing = false));
